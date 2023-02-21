971d84dfd268d377b4d05f4dce789a93fa75e274
Jun99uu_Lee <44965706+Jun99uu@users.noreply.github.com>
2023-02-22 01:05:38 +0900

    feat: add splash screen (#7)
    and expot navi-container from App.tsx

--- TERMTERM/src/screens/Login.tsx
+++ TERMTERM/src/screens/Login.tsx
@@ -1,24 +1,9 @@
 import styled from "styled-components/native";
 import { UrlText } from "@components/index";
-import { View, Text } from "react-native";
-// import { tmpToken } from "@style/textToken";
+import { View } from "react-native";
 
 const Login = () => {
-  return (
-    // <Container>
-    //   <View></View>
-    //   <View></View>
-    //   <UrlText
-    //     text={"이용하시는 데에 어려움이 있나요?"}
-    //     url=""
-    //     token={tmpToken}
-    //     color={`#858585`}
-    //   />
-    // </Container>
-    <View>
-      <Text>이준규 파이팅</Text>
-    </View>
-  );
+  return <View></View>;
 };
 
 export default Login;
