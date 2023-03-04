720acf8068e352c400cfef79eafef13269b6ce57
Jun99uu_Lee <igun0423@naver.com>
2023-03-04 17:05:45 +0900

    feat : add header named title-header

--- TERMTERM/src/components/common/NavigatorTitle.tsx
+++ TERMTERM/src/components/common/NavigatorTitle.tsx
@@ -1,16 +1,25 @@
 import styled from "styled-components/native";
 import { TEXT_STYLES } from "@style/designSystem";
 
+/**
+ * 헤더에 들어가는 타이틀
+ */
 export const NavigatorTitle = styled.Text`
   font-size: ${TEXT_STYLES.md2.Sb?.fontSize}px;
   font-weight: ${TEXT_STYLES.md2.Sb?.fontWeight};
 `;
 
+/**
+ * 헤더에 들어가는 현황 수
+ */
 export const NavigatorPager = styled.Text`
   font-size: 14px;
   font-weight: 400;
 `;
 
+/**
+ * 헤더에 들어가는 카렛버튼
+ */
 export const CaretBtn = styled.TouchableOpacity`
   width: 20px;
   height: 20px;
--- TERMTERM/src/components/header/TitleBar.tsx
+++ TERMTERM/src/components/header/TitleBar.tsx
@@ -1,5 +1,27 @@
+import NavigationBar from "../common/NavigationBar";
+import { NavigatorTitle, NavigatorPager } from "../common/NavigatorTitle";
+import styled from "styled-components/native";
+import { AntDesign } from "@expo/vector-icons";
+import { useThemeStyle } from "@hooks/useThemeStyle";
+import AutoSizedImage from "@components/common/AutoSizedImage";
+
 export interface Props {
-  children: React.ReactNode;
+  title: string;
+  /**
+   * 아이콘 종류
+   * "point" -> 포인트 아이콘
+   * "folder" -> 폴더 아이콘
+   * 전달 안하면 아이콘 없음
+   */
+  icon?: string;
+  /**
+   * 포인트 점수
+   */
+  point?: number;
+  /**
+   * 아이콘 클릭시 실행 될 함수
+   */
+  onPress?: () => void;
 }
 
 /**
@@ -7,8 +29,40 @@ export interface Props {
  * 자식 노드에 오른쪽에 위치한 요소를 배치할 수 있다.
  * 1~3번 헤더 컴포넌트
  */
-const TitleBar = ({ children }: Props) => {
-  return <>{children}</>;
+const TitleBar = ({ title, icon, point, onPress }: Props) => {
+  const [COLOR] = useThemeStyle();
+  return (
+    <NavigationBar>
+      <NavigatorTitle>{title}</NavigatorTitle>
+      {icon === "point" ? (
+        <PointBox>
+          <AutoSizedImage source={require("@assets/icon/point.png")} />
+          <NavigatorPager style={{ marginLeft: 5 }}>{point!}</NavigatorPager>
+        </PointBox>
+      ) : icon === "folder" ? (
+        <FolderBtn onPress={() => onPress!()}>
+          <AntDesign name="addfolder" size={24} color={COLOR.Text.active} />
+        </FolderBtn>
+      ) : (
+        <></>
+      )}
+    </NavigationBar>
+  );
 };
+
+const PointBox = styled.View`
+  display: flex;
+  height: 100%;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const FolderBtn = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default TitleBar;
