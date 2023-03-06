1df3c481c16d3f9bbfb724740388009486025d6a
Jun99uu_Lee <igun0423@naver.com>
2023-03-06 17:49:13 +0900

    fix : fix IconBar ui

--- TERMTERM/src/components/header/IconBar.tsx
+++ TERMTERM/src/components/header/IconBar.tsx
@@ -1,5 +1,9 @@
 import NavigationBar from "../common/NavigationBar";
-import { NavigatorTitle, CaretBtn } from "../common/NavigatorTitle";
+import {
+  NavigatorTitle,
+  CaretBtn,
+  TitleWrapper,
+} from "../common/NavigatorTitle";
 import { AntDesign } from "@expo/vector-icons";
 import { useThemeStyle } from "@hooks/useThemeStyle";
 import styled from "styled-components/native";
@@ -31,34 +35,45 @@ const IconBar = ({ curNum, maxNum, onBack, icon, onPress, onDots }: Props) => {
 
   const Fold = () => {
     return theme ? (
-      <AutoSizedImage source={require("@assets/icon/fold.png")} />
+      <AutoSizedImage source={require("@assets/icon/fold.png")} height={22} />
     ) : (
-      <AutoSizedImage source={require("@assets/icon/fold-dark.png")} />
+      <AutoSizedImage
+        source={require("@assets/icon/fold-dark.png")}
+        height={22}
+      />
     );
   };
 
   const Collapse = () => {
     return theme ? (
-      <AutoSizedImage source={require("@assets/icon/collapse.png")} />
+      <AutoSizedImage
+        source={require("@assets/icon/collapse.png")}
+        height={24}
+      />
     ) : (
-      <AutoSizedImage source={require("@assets/icon/collapse-dark.png")} />
+      <AutoSizedImage
+        source={require("@assets/icon/collapse-dark.png")}
+        height={24}
+      />
     );
   };
 
   return (
     <NavigationBar style={{ justifyContent: "space-between" }}>
-      <CaretBtn onPress={() => onBack()}>
+      <CaretBtn onPress={() => onBack()} style={{ marginLeft: 20 }}>
         <AntDesign name="left" size={24} color={COLOR.Text.active} />
       </CaretBtn>
       {maxNum && curNum ? (
-        <NavigatorTitle style={{ color: COLOR.Text.active }}>
-          {`${curNum}/${maxNum}`}
-        </NavigatorTitle>
+        <TitleWrapper>
+          <NavigatorTitle style={{ color: COLOR.Text.active }}>
+            {`${curNum}/${maxNum}`}
+          </NavigatorTitle>
+        </TitleWrapper>
       ) : (
         <></>
       )}
-      <ElementWrapper>
-        <CaretBtn onPress={() => onPress()}>
+      <ElementWrapper style={{ marginRight: 20 }}>
+        <CaretBtn onPress={() => onPress()} style={{ marginRight: 15 }}>
           {icon === Icon.fold ? <Fold /> : <Collapse />}
         </CaretBtn>
         <CaretBtn onPress={() => onDots()}>
