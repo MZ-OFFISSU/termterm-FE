56c35b59df8a2059ef200b663b110bb6cf004029
Jun99uu_Lee <igun0423@naver.com>
2023-03-07 11:45:59 +0900

    feat : place components in search screen

---
+++ TERMTERM/src/components/search/index.ts
@@ -0,0 +1,6 @@
+import LogWrapper from "./LogWrapper";
+import ContentsHeader from "./ContentsHeadeer";
+import ContentsWrapper from "./ContentsWrapper";
+import SearchBox from "./SearchBox";
+
+export { LogWrapper, ContentsWrapper, SearchBox, ContentsHeader };
--- TERMTERM/src/screens/Toolbar/Search.tsx
+++ TERMTERM/src/screens/Toolbar/Search.tsx
@@ -1,21 +1,48 @@
-import { TouchableOpacity, Text } from "react-native";
+import styled from "styled-components/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StackScreenProps } from "@react-navigation/stack";
 import { RootStackParamList } from "@interfaces/RootStackParamList";
+import { TitleBar } from "@components/header";
+import {
+  SearchBox,
+  ContentsWrapper,
+  ContentsHeader,
+  LogWrapper,
+} from "@components/search";
 
 export type Props = StackScreenProps<RootStackParamList, "ToolBar">;
 
+const dummy = ["기획", "루시", "바쁘거든", "채워", "바빠", "바빠유"];
+
 const Search = ({ navigation }: Props) => {
   return (
-    <SafeAreaView>
-      <TouchableOpacity
-        onPress={() => navigation.push("Curation")}
-        style={{ height: 3000 }}
-      >
-        <Text> 눌러라 그럼 열릴 것이다</Text>
-      </TouchableOpacity>
+    <SafeAreaView style={{ backgroundColor: "white" }}>
+      <Container stickyHeaderIndices={[0]}>
+        <TitleBar title="검색" />
+        <CotentsArea>
+          <SearchBox />
+          <ContentsWrapper>
+            <ContentsHeader title="최근 검색어" />
+            <LogWrapper logs={dummy} />
+          </ContentsWrapper>
+        </CotentsArea>
+      </Container>
     </SafeAreaView>
   );
 };
+
+const Container = styled.ScrollView`
+  width: 100%;
+  height: 100%;
+`;
+
+const CotentsArea = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 30px 16px;
+`;
 
 export default Search;
