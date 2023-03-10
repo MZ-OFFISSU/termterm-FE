d2ce69e3a5b64b3ae07dc2694a991036903e63cf
Jun99uu_Lee <igun0423@naver.com>
2023-03-11 02:46:19 +0900

    fix : fix navigation props in toolbar

--- TERMTERM/src/screens/ToolBar.tsx
+++ TERMTERM/src/screens/ToolBar.tsx
@@ -5,11 +5,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { Home, Search, Archive, My } from "./Toolbar/index";
 import { TEXT_STYLES } from "@style/designSystem";
+import { StackScreenProps } from "@react-navigation/stack";
+import { RootStackParamList } from "@interfaces/RootStackParamList";
+
+export type Props = StackScreenProps<RootStackParamList, "ToolBar">;
 
 /**
  * 툴바(바텀바) 컴포넌트
  */
-const ToolBar = () => {
+const ToolBar = ({ ...props }: Props) => {
   /** tabNavigator 생성 */
   const Tab = createBottomTabNavigator();
   const [COLOR] = useThemeStyle();
@@ -19,6 +23,7 @@ const ToolBar = () => {
       screenOptions={({ route }) => ({
         tabBarHideOnKeyboard: true,
         tabBarStyle: {
+          backgroundColor: COLOR.Background.surface,
           height: 65,
           paddingTop: 10,
           paddingBottom: 10,
@@ -48,10 +53,9 @@ const ToolBar = () => {
           ),
         }}
       />
-
       <Tab.Screen
         name="Search"
-        component={Search}
+        children={() => <Search {...props} />}
         options={{
           headerShown: false,
           title: "검색",
