24ab4d929344c1d6c4f8cb0bd0b8c16d1a6e5146
yoo-jimin127 <dbwlals9936@naver.com>
2023-04-12 17:45:28 +0900

    feat: word interface 추가

---
+++ TERMTERM/src/interfaces/word.ts
@@ -0,0 +1,15 @@
+import { Category, Comment } from "./term";
+
+/** main screen 등에서 사용되는 word 컴포넌트
+* (용어(한글), 용어(영어), 용어 설명, 용어 출처)
+*/
+export interface WordProps {
+    id: number,
+    korWord?: string,
+    engWord?: string,
+    description: string,
+    source: string,
+    bookmarked?: boolean,
+    categories: Array<Category>;
+    comments: Array<Comment>;
+}
