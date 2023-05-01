7874e9b6b19b367f66c4f79418d757106e8ef096
yoo-jimin127 <dbwlals9936@naver.com>
2023-05-01 12:23:32 +0900

    chore: support screen 테마 적용

--- TERMTERM/src/screens/Support.tsx
+++ TERMTERM/src/screens/Support.tsx
@@ -1,13 +1,6 @@
 import styled from "styled-components/native";
 import { LIGHT_COLOR_STYLE } from "@style/designSystem";
-import { First, Second, Third } from "./Onboarding/index";
-import {
-  NavigationBar,
-  NavigatorTitle,
-  NavigatorPager,
-  CaretBtn,
-} from "@components/index";
-import { AntDesign } from "@expo/vector-icons";
+import { First, Second, Third } from "./Support/index";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useThemeStyle } from "@hooks/useThemeStyle";
 import { useState } from "react";
@@ -33,26 +26,7 @@ const Support = ({ navigation }: Props) => {
       style={{ flex: 0, backgroundColor: COLOR.Background.surface }}
     >
       <Wrapper>
-        <NavigationBar
-          style={{
-            paddingLeft: 17,
-            paddingRight: 17,
-            justifyContent: "space-between",
-          }}
-        >
-          <CaretBtn onPress={() => navigation.pop()}>
-            <AntDesign name="left" size={20} color={COLOR.Text.active} />
-          </CaretBtn>
-          <NavigatorTitle style={{ color: COLOR.Text.active }}>
-            회원가입
-          </NavigatorTitle>
-          <NavigatorPager style={{ color: COLOR.Text.active }} COLOR={COLOR}>
-            {stage + 1} / 3
-          </NavigatorPager>
-        </NavigationBar>
-        <Contents>
-          <CurrentPage onEnd={() => onEnd()} />
-        </Contents>
+
       </Wrapper>
     </SafeAreaView>
   );
