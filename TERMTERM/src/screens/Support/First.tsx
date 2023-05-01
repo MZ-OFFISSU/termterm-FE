24b05684c596cca3188a3b5e7fb92aa2c6759e50
yoo-jimin127 <dbwlals9936@naver.com>
2023-05-01 12:07:23 +0900

    feat: 문의사항 각 페이지 초기 셋팅

---
+++ TERMTERM/src/screens/Support/First.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components/native";
+import { View, Keyboard } from "react-native";
+import { useState, useEffect } from "react";
+import { screenWidth } from "@style/dimensions";
+import { useRecoilState } from "recoil";
+import { themeState } from "@recoil/themeState";
+
+const First = () => {
+  const [theme, setTheme] = useRecoilState(themeState);
+  const [btnPosition, setBtnPosiition] = useState(30);
+
+  return (
+    <View
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        paddingTop: 80,
+      }}
+    >
+    </View>
+  );
+};
+
+export default First;
