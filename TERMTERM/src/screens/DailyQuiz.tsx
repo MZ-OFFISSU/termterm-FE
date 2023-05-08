26f3b0eafabf5132f4b246399a3aa07b42262729
yoo-jimin127 <dbwlals9936@naver.com>
2023-05-08 14:45:04 +0900

    chore: 데일리 퀴즈 스크린 레이아웃(TODO 수정 + 디버깅 필요)

--- TERMTERM/src/screens/DailyQuiz.tsx
+++ TERMTERM/src/screens/DailyQuiz.tsx
@@ -1,86 +1,128 @@
 import styled from "styled-components/native";
 import { useState, useEffect } from "react";
-import AutoSizedImage from "@components/common/AutoSizedImage";
 import { StackScreenProps } from "@react-navigation/stack";
 import { RootStackParamList } from "@interfaces/RootStackParamList";
-import { DARK_COLOR_STYLE, LIGHT_COLOR_STYLE, TEXT_STYLE_SIZE, TEXT_STYLE_WEIGHT } from "@style/designSystem";
+import { LIGHT_COLOR_STYLE, colorTheme } from "@style/designSystem";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { DailyQuizItemProps } from "@interfaces/dailyquiz";
 import QuizCard from "@components/quiz/QuizCard";
+import { useThemeStyle } from "@hooks/useThemeStyle";
 import { BackBar } from "@components/header";
-import { Button } from "react-native";
+import { dummyWord, dummyWords } from "@assets/dummyWord";
+import QuizButton from "@components/quiz/QuizButton";
+import { useWordReg } from "@hooks/useWordReg";
 
 export type Props = StackScreenProps<RootStackParamList, "DailyQuiz">;
 
-const dummy: Array<DailyQuizItemProps> = [
-    {
-        word: "가독성",
-        explain: "얼마나 쉽게 읽을 수 있는지를 나타내는 정도를 뜻하는 말이에요. 프로그래밍에서의 가독성은 소스코드를 보고 코드가 의도하는 동작이나 알고리즘을 얼마나 쉽게 이해할 수 있는지를 뜻해요.",
-    },
-    {
-        word: "Stakeholder",
-        explain: "지폐나 동전과 같은 실물이 없이 네트워크로 연결된 특정한 가상공간(vitual community)에서 전자적 형태로 사용되는 디지털 화폐 또는 전자화폐를 말해요."
-    },
-    {
-        word: "Product Manager",
-        explain: "얼마나 쉽게 읽을 수 있는지를 나타내는 정도를 뜻하는 말이에요. 프로그래밍에서의 가독성은 소스코드를 보고 코드가 의도하는 동작이나 알고리즘을 얼마나 쉽게 이해할 수 있는지를 뜻해요.",
-    },
-];
-
 const DailyQuiz = ({ navigation }: Props) => {
-    const [idx, setIdx] = useState(0);
-    const [isSelect, setSelect] = useState<unknown>({
-        id1: false,
-        id2: false,
-        id3: false,
-    });
+  const [idx, setIdx] = useState(0);
+  const [COLOR, mode] = useThemeStyle();
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState<number | null>(null);
+  const [words, setWords] = useState(dummyWords);
 
-    return (
-        <SafeAreaView style={{backgroundColor: "white"}}>
-            <Container>
-                <BackBar title={`${idx + 1}/5`} onBack={() => navigation.pop()} />
-                <Title>어떤 용어에 대한 설명일까요?</Title>
-                <QuizCard explain={dummy[idx].explain} />
-                {dummy.map((item, idx) => (
-                    <QuizButton
-                        onPress={() => navigation.push("CompleteQuiz")}
-                    >
-                        <ButtonText>{item.word}</ButtonText>
-                    </QuizButton>
-                ))}
-            </Container>
-        </SafeAreaView>
-    )
-}
+  const handlePress = () => {
+    if (selectedButtonIndex !== null) {
+      const isCorrect = words[idx].name === useWordReg(words[idx].name)[1];
+      if (isCorrect) {
+        setIdx(idx + 1);
+        if (idx < words.length - 1) {
+          setSelectedButtonIndex(selectedButtonIndex + 1);
+        }
+      }
+    }
+  };
 
-const Container = styled.ScrollView`
-    width: 100%;
-    height: 100%;
+  const getButtonText = (index: number) => {
+    const [sub, main] = useWordReg(words[idx].name);
+    if (index === 1) {
+      return main || "바보";
+    } else {
+      // TODO : 정답이 아닌 다른 값 랜덤으로 가져오는 기능 추가
+      return sub ? `Not ${main}` : `Button ${index}`;
+    }
+  };
+
+  useEffect(() => {
+    handlePress();
+  }, [selectedButtonIndex]);
+  
+
+  return (
+    <SafeAreaView style={{ marginTop: "-15%" }}>
+      <BackBar title={`${idx + 1}/5`} onBack={() => navigation.pop()} />
+      <Container COLOR={COLOR}>
+        <Title COLOR={COLOR} mode={mode}>
+          어떤&nbsp;
+          <Title 
+            COLOR={COLOR} 
+            mode={mode} 
+            style={{ fontWeight: "900" }}
+          >용어</Title>에 대한 설명일까요?
+        </Title>
+        <InnerContainer>
+          <QuizCard explain={words[idx].description} quiz={true} />
+          <ButtonContainer>
+            {Array.from({ length: 3 }, (_, index) => (
+              <QuizButton
+                key={index}
+                active={selectedButtonIndex === index}
+                onPress={() => setSelectedButtonIndex(index)}
+              >
+                <ButtonText COLOR={COLOR} mode={mode}>{getButtonText(index)}</ButtonText>
+              </QuizButton>
+            ))}
+          </ButtonContainer>
+        </InnerContainer>
+      </Container>
+    </SafeAreaView>
+  )
+};
+
+const Container = styled.ScrollView<{ COLOR: colorTheme}>`
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) => props.COLOR.Background.surface};
 `;
 
-const Title = styled.Text`
-    font-size: 23px;
-    font-weight: 600;
-    color: ${LIGHT_COLOR_STYLE.Text.active};
-    text-align: center;
-    margin: 40px auto;
+const InnerContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 10px 16px;
 `;
 
-const QuizButton = styled.TouchableOpacity`
-    width: 358px;
-    height: 47px;
-    border-radius: 10px;
-    border-width: 1px;
-    border-style: solid;
-    border-color: lightgray;
-    margin: 10px auto;
+const Title = styled.Text<{
+  COLOR: colorTheme;
+  mode: boolean;
+}>`
+  font-size: 23px;
+  font-weight: 400;
+  color: ${(props) =>
+    props.mode
+    ? props.COLOR.Text.darken
+    : props.COLOR.Text.lighten
+  };
+  text-align: center;
+  margin-bottom: 20px;
 `;
 
-const ButtonText = styled.Text`
-    color: ${LIGHT_COLOR_STYLE.Text.active};
-    font-size: 15px;
-    text-align: center;
-    margin: auto 0;
+const ButtonContainer = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 20px;
+`;
+
+const ButtonText = styled.Text<{
+  COLOR: colorTheme;
+  mode: boolean;
+}>`
+  color: ${(props) => props.COLOR.Text.active};
+  font-size: 15px;
+  text-align: center;
+  margin: auto 0;
 `;
 
 export default DailyQuiz;
