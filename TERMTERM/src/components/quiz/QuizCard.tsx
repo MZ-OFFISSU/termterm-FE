6abab44d298a7011678c060686f03ad5ed0840b3
yoo-jimin127 <dbwlals9936@naver.com>
2023-05-08 14:44:05 +0900

    chore: 데일리 퀴즈 카드 디자인시스템 반영

--- TERMTERM/src/components/quiz/QuizCard.tsx
+++ TERMTERM/src/components/quiz/QuizCard.tsx
@@ -8,40 +8,58 @@ import {
 } from "@style/designSystem";
 import { useThemeStyle } from "@hooks/useThemeStyle";
 import { colorTheme } from "@style/designSystem";
+import { screenWidth } from "@style/dimensions";
+import { PreventRemoveProvider } from "@react-navigation/native";
 
 interface QuizCardProps {
-  title?: string;
   explain: string;
+  title?: string;
+  quiz?: boolean;
 }
 
-const QuizCard = (props: QuizCardProps) => {
+const QuizCard = ({ explain, title, quiz, ...props }: QuizCardProps) => {
   const [COLOR, mode] = useThemeStyle();
 
   return (
-    <CardContainer>
-      <CardText>{props.explain}</CardText>
+    <CardContainer COLOR={COLOR} mode={mode} quiz={quiz} {...props}>
+      <CardText COLOR={COLOR} mode={mode}>{explain}</CardText>
     </CardContainer>
   );
 };
 
-const CardContainer = styled.View`
-  width: 358px;
-  height: 200px;
-  background-color: ${LIGHT_COLOR_STYLE.THEME.secondary[10]};
-  border-radius: 20px;
+const CardContainer = styled.View<{
+  COLOR: colorTheme;
+  mode: boolean;
+  quiz?: boolean;
+}>`
+  width: ${screenWidth - 32}px;
+  height: 242px;
+  background-color: ${(props) =>
+    props.mode
+      ? props.quiz
+        ? props.COLOR.THEME.secondary[10]
+        : props.COLOR.THEME.primary[10]
+      : props.COLOR.Background.onSurface};
+  border-radius: 10px;
   text-align: center;
   margin: 10px auto 40px auto;
-  padding: 10px;
+  padding: 0px 20px;
 `;
 
-const CardText = styled.Text`
-  color: ${LIGHT_COLOR_STYLE.Text.darken};
-  // font-size: ${TEXT_STYLE_SIZE.sm};
-  font-size: 16px;
+const CardText = styled.Text<{
+  COLOR: colorTheme;
+  mode: boolean;
+}>`
+  color: ${(props) => 
+    props.mode 
+    ? props.COLOR.Text.darken 
+    : props.COLOR.Text.lighten
+  };
+  font-size: ${TEXT_STYLE_SIZE.sm}px;
   font-weight: ${TEXT_STYLE_WEIGHT.Reg};
   line-height: 25px;
-  text-align: center;
-  margin: auto 0;
+  text-align: left;
+  margin: 30px 10px;
 `;
 
 export default QuizCard;
