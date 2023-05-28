bac56f59320a8f2ab3a9236eaefd518f01b548a8
yoo-jimin127 <dbwlals9936@naver.com>
2023-05-28 22:27:25 +0900

    chore: walkthrough page

--- TERMTERM/src/screens/Walkthrough.tsx
+++ TERMTERM/src/screens/Walkthrough.tsx
@@ -13,6 +13,8 @@ import CustomButton, {
   BUTTON_STATE,
   BUTTON_TYPE,
 } from "@components/buttons/CustomButton";
+import { WordCarousel } from "@components/terms";
+import WalkthroughCarousel from "@components/terms/WalkthroughCarousel";
 
 export type Props = StackScreenProps<RootStackParamList, "Walkthrough">;
 interface WtProps {
@@ -20,6 +22,81 @@ interface WtProps {
   children: React.ReactNode;
   image: any;
 }
+
+const Walkthrough = ({ navigation }: Props) => {
+  const [COLOR, mode] = useThemeStyle();
+  const [walkthough, setWalkthrough] = useState<Array<WtProps> | null>();
+  const [width, setWidth] = useState(300);
+
+  useEffect(() => {
+    setWalkthrough(walkthroughInfo);
+  }, []);
+
+  /** 아이콘 너비 계산 함수 */
+  const calcWidth = () => {
+    if (screenWidth < 390) return;
+    if (screenWidth < 435) {
+      setWidth(340);
+      return;
+    }
+    if (screenWidth < 500) {
+      setWidth(380);
+      return;
+    }
+    if (screenWidth > 500) {
+      setWidth(420);
+      return;
+    }
+  };
+  useEffect(() => {
+    calcWidth();
+  }, []);
+
+  return (
+    <Wrapper>
+      <WalkthroughCarousel walkthrough={walkthroughInfo} dots={true} />
+      <ButtonWrapper>
+        <CustomButton
+          title="시작하기"
+          theme={mode}
+          type={BUTTON_TYPE.primary}
+          state={BUTTON_STATE.active}
+          // TODO : Home으로 이동하도록 navigation 수정
+          style={{ width: "90%" }}
+        />
+        <CustomButton
+          title="다시 보지 않기"
+          theme={mode}
+          type={BUTTON_TYPE.primary}
+          state={BUTTON_STATE.default}
+          // TODO : 다시 보지 않기 로직 추가
+          style={{ width: "90%", marginTop: "3%" }}
+        />
+      </ButtonWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.View`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background-color: ${LIGHT_COLOR_STYLE.Background.surface};
+  align-items: center;
+  position: relative;
+`;
+
+const ButtonWrapper = styled.View`
+  width: 100%;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  bottom: 10%;
+`;
+
+export default Walkthrough;
 
 const walkthroughInfo: Array<WtProps> = [
   {
@@ -65,114 +142,3 @@ const walkthroughInfo: Array<WtProps> = [
     image: require("../../assets/walkthrough/point.png"),
   },
 ];
-
-const Walkthrough = ({ navigation }: Props) => {
-  const [COLOR, mode] = useThemeStyle();
-  const [step, setStep] = useState(0);
-  const [width, setWidth] = useState(300);
-
-  const nextStep = () => {
-    if (step < walkthroughInfo.length - 1) {
-      setStep(step + 1);
-    } else {
-      /** 마지막 스텝에서 Home으로 이동하도록 */
-      navigation.navigate("Home");
-    }
-  };
-
-  const previousStep = () => {
-    if (step > 0) {
-      setStep(step - 1);
-    }
-  };
-
-  useEffect(() => {
-    // TODO : step 변경에 따른 처리 코드
-  }, [step]);
-
-  /** 아이콘 너비 계산 함수 */
-  const calcWidth = () => {
-    if (screenWidth < 390) return;
-    if (screenWidth < 435) {
-      setWidth(340);
-      return;
-    }
-    if (screenWidth < 500) {
-      setWidth(380);
-      return;
-    }
-    if (screenWidth > 500) {
-      setWidth(420);
-      return;
-    }
-  };
-  useEffect(() => {
-    calcWidth();
-  }, []);
-
-  return (
-    <Wrapper>
-      <Title>{walkthroughInfo[step].title}</Title>
-      <SubText>{walkthroughInfo[step].children}</SubText>
-      <AutoSizedImage
-        source={walkthroughInfo[step].image}
-        width={width}
-        style={{ marginTop: "8%" }}
-      />
-      <CustomButton
-        title="시작하기"
-        theme={mode}
-        type={BUTTON_TYPE.primary}
-        state={BUTTON_STATE.active}
-        // TODO : Home으로 이동하도록 navigation 수정
-        onPress={nextStep}
-        style={{ width: "90%" }}
-      />
-      <CustomButton
-        title="다시 보지 않기"
-        theme={mode}
-        type={BUTTON_TYPE.primary}
-        state={BUTTON_STATE.default}
-        // TODO : 다시 보지 않기 로직 추가
-        onPress={previousStep}
-        style={{ width: "90%", marginTop: "3%" }}
-      />
-    </Wrapper>
-  );
-};
-
-const Wrapper = styled.View`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  background-color: ${LIGHT_COLOR_STYLE.Background.surface};
-  align-items: center;
-  position: relative;
-`;
-
-const Title = styled.Text`
-  color: ${LIGHT_COLOR_STYLE.THEME.primary[130]};
-  font-size: 21px;
-  font-weight: 800;
-  margin-top: 30px;
-`;
-
-const SubTitle = styled.Text<{ type: "highlight" | "normal" }>`
-  color: ${LIGHT_COLOR_STYLE.Text.active};
-  font-size: 24px;
-  font-weight: ${(props) => (props.type === "highlight" ? "900" : "400")};
-  text-align: center;
-  line-height: 40px;
-`;
-
-const SubText = styled.Text`
-  color: ${LIGHT_COLOR_STYLE.Text.active};
-  font-size: 24px;
-  font-weight: 500;
-  color: ${LIGHT_COLOR_STYLE.Text.active};
-  text-align: center;
-  margin: 30px 0;
-  line-height: 30px;
-`;
-
-export default Walkthrough;
