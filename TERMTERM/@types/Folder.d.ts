4e77897469777acaa52124a773afff651b535c56
yoo-jimin127 <dbwlals9936@naver.com>
2023-08-09 14:32:08 +0900

    feat: 폴더 관련 `d.ts` 파일 & 엔드포인트 API 통신 객체 선언(디버깅 필요)

---
+++ TERMTERM/@types/Folder.d.ts
@@ -0,0 +1,61 @@
+import { TermComment, TermPreview } from "Term";
+
+declare module "Folder" {
+  export type FolderDetail = {
+    bookmarked: string;
+    categories: string[];
+    comments: TermComment[];
+    description: string;
+    id: number;
+    name: string;
+    source: string;
+  };
+
+  export type FolderPreview = {
+    currentCount: number;
+    description: string;
+    folderId: number;
+    saveLimit: number;
+    terms: TermPreview[];
+    title: string;
+  };
+
+  export type EditFolder = {
+    description: string;
+    folderId: number;
+    name: string;
+  };
+
+  export type FolderList = {
+    folderId: number;
+    title: string;
+  };
+
+  export type FolderInfo = {
+    folderId: number;
+    folderName: string;
+  };
+
+  export type CreateFolder = {
+    description: string;
+    title: string;
+  };
+
+  export type CancelArchive = {
+    folderId: number;
+    termId: number;
+  };
+
+  export type FolderModal = {
+    currentFolderCount: number;
+    myFolderCreationLimit: number;
+    systemFolderCreationLimit: number;
+  };
+
+  export type RandomTerms = {
+    description: string;
+    name: string;
+    source: string;
+    termId: string;
+  };
+}
---
+++ TERMTERM/src/api/FolderApi.ts
@@ -0,0 +1,76 @@
+import {
+  FolderDetail,
+  FolderPreview,
+  EditFolder,
+  FolderList,
+  CreateFolder,
+  FolderInfo,
+  FolderModal,
+  CancelArchive,
+  RandomTerms,
+} from "Folder";
+import { get, post, put, remove } from "./AxiosCreate";
+
+class FolderApi {
+  /** 폴더 삭제 */
+  removeFolder = async (id: number): Promise<string> => {
+    const data = await remove<string>(`/v1/folder/${id}`);
+    return data;
+  };
+
+  /** 폴더 상세 페이지_하나씩 보기 */
+  getEachFolderDetail = async (id: number): Promise<FolderDetail> => {
+    const data = await get<FolderDetail>(`/v1/folder/detail/each/${id}`);
+    return data;
+  };
+
+  /** 폴더 상세 페이지_모아서 보기 */
+  getSumFolderDetail = async (id: number): Promise<FolderPreview> => {
+    const data = await get<FolderPreview>(`/v1/folder/detail/sum/${id}`);
+    return data;
+  };
+
+  /** 폴더 정보 수정 */
+  putFolderInfo = async (folderInfo: EditFolder): Promise<string> => {
+    const data = await put<string>(`/v1/folder/info`, folderInfo);
+    return data;
+  };
+
+  /** 내 폴더 리스트 */
+  getMyFolderList = async (): Promise<FolderList> => {
+    const data = await get<FolderList>(`/v1/folder/list`);
+    return data;
+  };
+
+  /** 폴더 생성 */
+  registerFolder = async (folderInfo: CreateFolder): Promise<FolderInfo> => {
+    const data = await post<FolderInfo>(`/v1/folder/new`, folderInfo);
+    return data;
+  };
+
+  /** 폴더 관련 정보 모달 */
+  getFolderModal = async (): Promise<FolderModal> => {
+    const data = await get<FolderModal>(`/v1/folder/related-info`);
+    return data;
+  };
+
+  /** 폴더에 용어 저장 (아카이빙) */
+  registerTermInFolder = async (folderIds: number[]): Promise<string> => {
+    const data = await post<string>(`/v1/folder/term`, folderIds);
+    return data;
+  };
+
+  /** 폴더에 용어 삭제 (아카이빙 해제) */
+  removeTermInFolder = async (
+    cancelArchiveInfo: CancelArchive
+  ): Promise<string> => {
+    const data = await remove<string>(`/v1/folder/term`, cancelArchiveInfo);
+    return data;
+  };
+
+  /** 홈 화면 - 아카이빙 한 단어들 중 10개를 랜덤으로 뽑아 리턴 */
+  getRandomArchiveTerms = async (): Promise<RandomTerms> => {
+    const data = await get<RandomTerms>(`/v1/folder/term/random-10`);
+    return data;
+  };
+}
