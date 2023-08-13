2d7d7e9db4b75fc68b608d61f2e5d1205b0521ff
yoo-jimin127 <dbwlals9936@naver.com>
2023-08-13 13:53:25 +0900

    feat: 용어 퀴즈 리뷰 API 연동(응답 값 나올 시 더미 삭제)

--- TERMTERM/src/screens/QuizReview.tsx
+++ TERMTERM/src/screens/QuizReview.tsx
@@ -1,16 +1,37 @@
+import QuizApi from "@api/QuizApi";
 import QuizReviewRouter from "@components/quiz/QuizReviewRouter";
 import { useThemeStyle } from "@hooks/useThemeStyle";
 import { quizReviewProps } from "@interfaces/quizReview";
 import { RootStackParamList } from "@interfaces/RootStackParamList";
 import { StackScreenProps } from "@react-navigation/stack";
 import { colorTheme, TYPO_STYLE } from "@style/designSystem";
+import { QuizReviewDetail } from "Quiz";
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
 export type Props = StackScreenProps<RootStackParamList, "QuizReview">;
 
 const QuizReview = ({ navigation }: Props) => {
+  const quizApi = new QuizApi();
   const [COLOR, mode] = useThemeStyle();
+  const [quizReviewList, setQuizReviewList] = useState<QuizReviewDetail[]>([]);
+  const [isAnswer, setIsAnswer] = useState<boolean[]>([]);
+
+  const getQuizReviewList = async () => {
+    try {
+      // TODO : Quiz 부분 검토 완료 시 확인
+      setQuizReviewList(await quizApi.getFinalQuizReview());
+      const answerArray: boolean[] = quizReviewList.map(
+        (item) => item.isAnswerRight
+      );
+      setIsAnswer(answerArray);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => console.log(getQuizReviewList()), []);
 
   return (
     <SafeAreaView style={{ backgroundColor: COLOR.Background.surface }}>
@@ -18,11 +39,11 @@ const QuizReview = ({ navigation }: Props) => {
         {dummy.map((item, idx) => (
           <QuizReviewRouter
             key={idx}
-            id={item.id}
-            answer={item.answer}
-            wordAnswer={item.wordAnswer}
-            userAnswer={item.userAnswer}
-            onPress={() => navigation.navigate("QuizResult", {id: 0})}
+            id={item.termId}
+            answer={item.isAnswerRight}
+            wordAnswer={item.termName}
+            userAnswer={item.wrongChoices[0]}
+            onPress={() => navigation.navigate("QuizResult", { id: 0 })}
           />
         ))}
       </Container>
@@ -38,35 +59,51 @@ const Container = styled.ScrollView`
   margin-top: 10px;
 `;
 
-const dummy: Array<quizReviewProps> = [
+/** TODO : 비어있는 퀴즈 리뷰 데이터 대체 임시 데이터 */
+const dummy: Array<QuizReviewDetail> = [
   {
-    id: 1,
-    answer: true,
-    wordAnswer: "Stakeholder",
-    userAnswer: "Stakeholder",
+    bookmarked: "NO",
+    isAnswerRight: true,
+    termDescription: "이 용어는 프로덕트 매니저입니다.",
+    termId: 1,
+    termName: "Product Manager",
+    termSource: "https://www.naver.com",
+    wrongChoices: ["Product Manager"],
   },
   {
-    id: 2,
-    answer: false,
-    wordAnswer: "Modal",
-    userAnswer: "Kick off",
+    bookmarked: "YES",
+    isAnswerRight: false,
+    termDescription: "이 용어는 모달입니다.",
+    termId: 2,
+    termName: "Modal",
+    termSource: "https://www.google.com",
+    wrongChoices: ["Slide Menu"],
   },
   {
-    id: 3,
-    answer: false,
-    wordAnswer: "Kick off",
-    userAnswer: "Modal",
+    bookmarked: "YES",
+    isAnswerRight: true,
+    termDescription: "이 용어는 스크럼입니다.",
+    termId: 3,
+    termName: "Scrum",
+    termSource: "https://www.daum.com",
+    wrongChoices: ["Scrum"],
   },
   {
-    id: 4,
-    answer: true,
-    wordAnswer: "API",
-    userAnswer: "API",
+    bookmarked: "NO",
+    isAnswerRight: true,
+    termDescription: "이 용어는 깃허브입니다.",
+    termId: 4,
+    termName: "Github",
+    termSource: "https://www.meta.com",
+    wrongChoices: ["Moda"],
   },
   {
-    id: 5,
-    answer: false,
-    wordAnswer: "Design System",
-    userAnswer: "Design Guide",
+    bookmarked: "YES",
+    isAnswerRight: false,
+    termDescription: "이 용어는 딤드입니다.",
+    termId: 5,
+    termName: "Dimmed",
+    termSource: "https://www.instagram.com",
+    wrongChoices: ["Dimmed"],
   },
 ];
