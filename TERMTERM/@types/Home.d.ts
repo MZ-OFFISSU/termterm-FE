16112c63587ea73aa06d03ca452e1749721aaffe
yoo-jimin127 <dbwlals9936@naver.com>
2023-08-14 12:25:48 +0900

    feat: `Home` 관련 통신객체 & 타입 정의

---
+++ TERMTERM/@types/Home.d.ts
@@ -0,0 +1,10 @@
+declare module "Home" {
+  export type HomeUxWriting = {
+    mainTitle: string;
+    subTitle: string;
+  };
+
+  export type SubTitles = {
+    subtitles: string[];
+  };
+}
---
+++ TERMTERM/src/api/HomeApi.ts
@@ -0,0 +1,31 @@
+import { HomeUxWriting, SubTitles } from "Home";
+import { get, post, remove } from "./AxiosCreate";
+
+class HomeApi {
+  /** 서브타이틀 리스트 */
+  getSubTitleList = async (): Promise<SubTitles> => {
+    // TODO : password 넘기기
+    const data = await get<SubTitles>(`/v1/home/subtitle`);
+    return data;
+  };
+
+  /** 서브타이틀 등록 (관리자용) */
+  registerSubTitle = async (): Promise<string> => {
+    // TODO : password 넘기기
+    const data = await post<string>(`/v1/home/subtitle`);
+    return data;
+  };
+
+  /** 서브타이틀 삭제 (관리자용) */
+  removeSubTitle = async (): Promise<string> => {
+    // TODO : password 넘기기
+    const data = await remove<string>(`/v1/home/subtitle`);
+    return data;
+  };
+
+  /** 홈 화면 상단 UX Writing */
+  getUxWriting = async (): Promise<HomeUxWriting> => {
+    const data = await get<HomeUxWriting>(`/v1/home/title`);
+    return data;
+  };
+}
