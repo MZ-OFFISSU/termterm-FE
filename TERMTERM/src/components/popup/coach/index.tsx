7291cbefa06b59b14f62eae626013fbf4ce8853c
Jun99uu_Lee <igun0423@naver.com>
2023-11-10 18:41:02 +0900

    fix: close button position

--- TERMTERM/src/components/popup/coach/index.tsx
+++ TERMTERM/src/components/popup/coach/index.tsx
@@ -34,29 +34,39 @@ const Coachmark = ({
       animationIn={"bounceIn"}
       animationOut={"bounceOut"}
     >
-      <CloseButtpn onPress={() => hideCoach(type, checked)}>
-        <AntDesign name="close" size={24} color={COLOR.Neutral[30]} />
-      </CloseButtpn>
-      <HideCheckbox onPress={handleCheck} up={type === "comment"}>
-        <MaterialCommunityIcons
-          name={checked ? "checkbox-marked" : `checkbox-blank-outline`}
-          size={20}
-          color="white"
-        />
-        <HideText>다시 보지 않기</HideText>
-      </HideCheckbox>
+      <CloseWrapper up={type === "comment"}>
+        <HideCheckbox onPress={handleCheck}>
+          <MaterialCommunityIcons
+            name={checked ? "checkbox-marked" : `checkbox-blank-outline`}
+            size={20}
+            color="white"
+          />
+          <HideText>다시 보지 않기</HideText>
+        </HideCheckbox>
+        <CloseButton onPress={() => hideCoach(type, checked)}>
+          <AntDesign name="close" size={24} color={COLOR.Neutral[30]} />
+        </CloseButton>
+      </CloseWrapper>
     </Modal>
   );
 };
 
-const HideCheckbox = styled.TouchableOpacity<{ up: boolean }>`
+const CloseWrapper = styled.View<{ up: boolean }>`
+  width: 100%;
+  position: absolute;
+  bottom: ${(props) => (props.up ? `${screenHeight - 100}px` : "70px")};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HideCheckbox = styled.TouchableOpacity`
   width: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  position: absolute;
-  bottom: ${(props) => (props.up ? `${screenHeight - 100}px` : "70px")};
 `;
 
 const HideText = styled.Text`
@@ -64,10 +74,15 @@ const HideText = styled.Text`
   margin-left: 5px;
 `;
 
-const CloseButtpn = styled.TouchableOpacity`
-  position: absolute;
-  top: 10px;
-  left: 10px;
+const CloseButton = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #5b5b5b36;
+  border-radius: 10px;
+  margin-top: 30px;
 `;
 
 export default Coachmark;
