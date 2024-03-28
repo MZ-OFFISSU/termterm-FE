5f32f0ac9f9ab715a31fdd2b0a3f9d3f3e750840
yoo-jimin127 <dbwlals9936@naver.com>
2024-03-28 16:31:34 +0900

    refactor : term 관련 v2 이관/보안설정

--- TERMTERM/src/api/TermApi.ts
+++ TERMTERM/src/api/TermApi.ts
@@ -11,19 +11,19 @@ import { AxiosRequestConfig } from "axios";
 class TermApi {
   /** 용어 북마크 (임시)*/
   bookmarkTerm = async (id: number): Promise<string> => {
-    const data = await post<string>(`/v1/term/bookmark/${id}`);
+    const data = await post<string>(`/v2/s/term/bookmark/${id}`);
     return data;
   };
 
   /** 오늘의 용어*/
   dailyTerm = async (): Promise<TermItem[]> => {
-    const data = await get<TermItem[]>(`/v1/term/daily`);
+    const data = await get<TermItem[]>(`/v2/s/term/daily`);
     return data;
   };
 
   /** 용어 상세 */
   termDetail = async (id: number): Promise<TermDetail> => {
-    const data = await get<TermDetail>(`/v1/term/detail/${id}`);
+    const data = await get<TermDetail>(`/v2/s/term/detail/${id}`);
     return data;
   };
 
@@ -32,13 +32,13 @@ class TermApi {
     categories: TermConfig,
     config?: AxiosRequestConfig
   ): Promise<TermResponse> => {
-    const data = await post<TermResponse>(`/v1/term/list`, categories, config);
+    const data = await post<TermResponse>(`/v2/s/term/list`, categories, config);
     return data;
   };
 
   /** 용어 검색 */
   searchTerm = async (token: string): Promise<SearchResult[]> => {
-    const data = await get<SearchResult[]>(`/v1/term/search/${token}`);
+    const data = await get<SearchResult[]>(`/v2/s/term/search/${token}`);
     return data;
   };
 }
