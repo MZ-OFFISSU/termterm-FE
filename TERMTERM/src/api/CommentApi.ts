99f3ce11c10c834c9152a459ad2d43afb32cd0a6
yoo-jimin127 <dbwlals9936@naver.com>
2024-03-28 16:30:28 +0900

    refactor : comment 관련 v2 이관/보안설정

--- TERMTERM/src/api/CommentApi.ts
+++ TERMTERM/src/api/CommentApi.ts
@@ -4,37 +4,37 @@ import { post, put } from "./AxiosCreate";
 class CommentApi {
   /**나만의 용어 신청 작성 후 등록 */
   registerComment = async (comment: CommentInput): Promise<string> => {
-    const data = await post<string>(`/v1/comment`, comment);
+    const data = await post<string>(`/v2/s/comment`, comment);
     return data;
   };
 
   /** (관리자) 나만의 용어 신청 승인 */
   acceptComment = async (id: number): Promise<string> => {
-    const data = await put<string>(`/v1/comment/accept/${id}`);
+    const data = await put<string>(`/v2/s/comment/accept/${id}`);
     return data;
   };
 
   /** 코멘트 좋아요 취소 */
   dislikeComment = async (id: number): Promise<string> => {
-    const data = await put<string>(`/v1/comment/dislike/${id}`);
+    const data = await put<string>(`/v2/s/comment/dislike/${id}`);
     return data;
   };
 
   /** 코멘트 좋아요 */
   likeComment = async (id: number): Promise<string> => {
-    const data = await put<string>(`/v1/comment/like/${id}`);
+    const data = await put<string>(`/v2/s/comment/like/${id}`);
     return data;
   };
 
   /** 코멘트 신고하기 */
   reportComment = async (report: Report): Promise<string> => {
-    const data = await post<string>(`/v1/comment/report`, report);
+    const data = await post<string>(`/v2/s/comment/report`, report);
     return data;
   };
 
   /** 코멘트 신고 승인 */
   acceptReport = async (id: number): Promise<string> => {
-    const data = await put<string>(`/v1/comment/report/completed/${id}`);
+    const data = await put<string>(`/v2/s/comment/report/completed/${id}`);
     return data;
   };
 }
